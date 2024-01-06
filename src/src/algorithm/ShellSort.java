92092efbc8f7474ac5038b0915bab4ce94c826b3
IK32K3 <146173061+IK32K3@users.noreply.github.com>
2024-01-06 15:08:09 +0700

    Update

--- src/src/algorithm/ShellSort.java
+++ src/src/algorithm/ShellSort.java
@@ -1,6 +1,8 @@
+package algorithm;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+import data.Unit;
 import javafx.animation.FillTransition;
 import javafx.animation.ParallelTransition;
 import javafx.animation.Transition;
@@ -16,7 +18,7 @@ public class ShellSort extends Sort{
 		this.transitions = new ArrayList<>();
 	}
 	
-	 private ParallelTransition colorCNodes(CNode[] arr, Color color, int end, int gap) {
+	 private ParallelTransition colorUnits(Unit[] arr, Color color, int end, int gap) {
 		    ParallelTransition pt = new ParallelTransition();
 		    
 		    while(end >= 0) {
@@ -30,7 +32,7 @@ public class ShellSort extends Sort{
 		    return pt;
 		  }
 	
-	private void shellSort(CNode[] arr) {
+	private void shellSort(Unit[] arr) {
 		// Start with a big gap, then reduce the gap 
 		for(int gap = arr.length/2; gap > 0; gap /= 2) {
 			// Do a gapped insertion sort for this gap size. 
@@ -38,27 +40,27 @@ public class ShellSort extends Sort{
 	        // keep adding one more element until the entire array is 
 	        // gap sorted  
 			for(int i = gap; i < arr.length; i++) {
-				 transitions.add(colorCNodes(arr, SUB_COLOR, i, gap));
+				 transitions.add(colorUnits(arr, SUB_COLOR, i, gap));
 				 for(int j = i;j >= gap; j -= gap) {
-					 transitions.add(colorCNode(arr, SELECT_COLOR, j - gap, j));
+					 transitions.add(colorUnit(arr, SELECT_COLOR, j - gap, j));
 					 if(arr[j-gap].getValue() > arr[j].getValue()) {
 						 transitions.add(swap(arr,j-gap,j));
-						 transitions.add(colorCNode(arr, SUB_COLOR, j - gap, j));
+						 transitions.add(colorUnit(arr, SUB_COLOR, j - gap, j));
 					 }
 					 else {
-						 transitions.add(colorCNode(arr, SUB_COLOR, j - gap, j));
+						 transitions.add(colorUnit(arr, SUB_COLOR, j - gap, j));
 						 break;
 					 }
 				 }
-				 transitions.add(colorCNodes(arr, START_COLOR, i, gap));
+				 transitions.add(colorUnits(arr, START_COLOR, i, gap));
 			}
 		}
 	}
 	
 	@Override
-	public ArrayList<Transition> sorting(CNode[] arr) {
+	public ArrayList<Transition> sorting(Unit[] arr) {
 		shellSort(arr);
-		this.transitions.add(colorCNode(Arrays.asList(arr), SORTED_COLOR));
+		this.transitions.add(colorUnit(Arrays.asList(arr), SORTED_COLOR));
 		return this.transitions;
 	}
 
