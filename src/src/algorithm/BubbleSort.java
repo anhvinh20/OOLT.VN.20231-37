ea3136c8adeeba5449d0be7dfd8bf52353cb68a8
IK32K3 <146173061+IK32K3@users.noreply.github.com>
2024-01-06 15:07:36 +0700

    Update BubbleSort.java

--- src/src/algorithm/BubbleSort.java
+++ src/src/algorithm/BubbleSort.java
@@ -1,7 +1,9 @@
+package algorithm;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 
+import data.Unit;
 import javafx.animation.Transition;
 
 public class BubbleSort extends Sort {
@@ -10,30 +12,30 @@ public class BubbleSort extends Sort {
   private ArrayList<Transition> transitions;
 
   public BubbleSort(double DX) {
-    super(DX);
+	super(DX);  
     this.transitions = new ArrayList<>();
   }
 
-  private ArrayList<Transition> compareCNode(CNode[] arr, int a, int b) {
+  private ArrayList<Transition> compareUnit(Unit[] arr, int a, int b) {
     ArrayList<Transition> transitions = new ArrayList<>();
 
-    transitions.add(colorCNode(arr, SELECT_COLOR, a, b));
+    transitions.add(colorUnit(arr, SELECT_COLOR, a, b));
 
     if (arr[a].getValue() > arr[b].getValue()) {
       transitions.add(swap(arr, a, b));
       swapped = true;
     } 
 
-    transitions.add(colorCNode(arr, START_COLOR, a, b));
+    transitions.add(colorUnit(arr, START_COLOR, a, b));
 
     return transitions;
   }
 
-  private void bubbleSort(CNode[] arr) {
+  private void bubbleSort(Unit[] arr) {
     for (int i = 0; i < arr.length; i++) {
       swapped = false;
       for (int j = 0; j < arr.length - 1 - i; j++) {
-        this.transitions.addAll(compareCNode(arr, j, j + 1));
+        this.transitions.addAll(compareUnit(arr, j, j + 1));
       }
 
       if (!swapped) {
@@ -44,10 +46,10 @@ public class BubbleSort extends Sort {
   }
 
   @Override
-  public ArrayList<Transition> sorting(CNode[] arr) {
+  public ArrayList<Transition> sorting(Unit[] arr) {
     bubbleSort(arr);
 
-    this.transitions.add(colorCNode(Arrays.asList(arr), SORTED_COLOR));
+    this.transitions.add(colorUnit(Arrays.asList(arr), SORTED_COLOR));
 
     return this.transitions;
 
