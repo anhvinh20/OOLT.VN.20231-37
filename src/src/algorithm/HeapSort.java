662222a6f8e36cff3125ff5746609e91394afd1b
IK32K3 <146173061+IK32K3@users.noreply.github.com>
2024-01-06 15:07:52 +0700

    Update HeapSort.java

--- src/src/algorithm/HeapSort.java
+++ src/src/algorithm/HeapSort.java
@@ -1,6 +1,9 @@
+package algorithm;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+
+import data.Unit;
 import javafx.animation.Transition;
 import javafx.scene.paint.Color;
 
@@ -10,11 +13,11 @@ public class HeapSort extends Sort {
   private ArrayList<Transition> transitions;
 
   public HeapSort(double DX) {
-    super(DX);
+	super(DX);  
     this.transitions = new ArrayList<>();
   }
 
-  private void heapify(CNode[] arr, int i, int n) {
+  private void heapify(Unit[] arr, int i, int n) {
     int left = 2 * i + 1;
     int right = 2 * i + 2;
     int max = i;
@@ -34,27 +37,27 @@ public class HeapSort extends Sort {
 
   }
 
-  private void heapSort(CNode[] arr) {
+  private void heapSort(Unit[] arr) {
     //build initial max heap
-    transitions.add(colorCNode(selectSubTree(arr, arr.length), SELECT_COLOR));
+    transitions.add(colorUnit(selectSubTree(arr, arr.length), SELECT_COLOR));
     for (int i = arr.length / 2 - 1; i >= 0; i--) {
       heapify(arr, i, arr.length);
     }
-    transitions.add(colorCNode(selectSubTree(arr, arr.length), START_COLOR));
+    transitions.add(colorUnit(selectSubTree(arr, arr.length), START_COLOR));
 
     //swap root node with final elt, heapify subarray
     for (int i = arr.length - 1; i > 0; i--) {
-      transitions.add(colorCNode(arr, ROOT_COLOR, 0));
+      transitions.add(colorUnit(arr, ROOT_COLOR, 0));
       transitions.add(swap(arr, 0, i));
-      transitions.add(colorCNode(arr, START_COLOR, i));
-      transitions.add(colorCNode(selectSubTree(arr, i), SELECT_COLOR));
+      transitions.add(colorUnit(arr, START_COLOR, i));
+      transitions.add(colorUnit(selectSubTree(arr, i), SELECT_COLOR));
       heapify(arr, 0, i);
-      transitions.add(colorCNode(selectSubTree(arr, i), START_COLOR));
+      transitions.add(colorUnit(selectSubTree(arr, i), START_COLOR));
     }
   }
 
-  private ArrayList<CNode> selectSubTree(CNode[] arr, int n) {
-    ArrayList<CNode> list = new ArrayList<>();
+  private ArrayList<Unit> selectSubTree(Unit[] arr, int n) {
+    ArrayList<Unit> list = new ArrayList<>();
 
     for (int i = 0; i < n; i++) {
       list.add(arr[i]);
@@ -64,10 +67,10 @@ public class HeapSort extends Sort {
   }
 
   @Override
-  public ArrayList<Transition> sorting(CNode[] arr) {
+  public ArrayList<Transition> sorting(Unit[] arr) {
     heapSort(arr);
 
-    transitions.add(colorCNode(Arrays.asList(arr), Color.ROYALBLUE));
+    transitions.add(colorUnit(Arrays.asList(arr), SORTED_COLOR));
     return transitions;
   }
 }
